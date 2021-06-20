772688a85979237f4e323832d40df71691302945
stanochka <krozzochka@gmail.com>
2021-06-20 12:49:27 +0300

    Add checking result and claiming the winner

--- script.js
+++ script.js
@@ -1,39 +1,74 @@
 //module
 const Gameboard = (() => {
-  const array = [];
+  let array = ['', '', '', '', '', '', '', '', ''];
+  let winner;
   const board = document.querySelectorAll('#gameboard>.cell');
-  const move = (turn) => {
-    board.forEach((div) => {
+  const result = document.querySelector('#result');
+  const play = (turn) => {
+    board.forEach(div => {
       div.addEventListener('click', () => {
         if (turn === 'x') {
           if (!div.textContent) {
             div.textContent = 'x';
             turn = 'o';
-            array.push({position: div.id, value: div.textContent});
+            array.splice(div.id, 1, div.textContent);
           };
         } else {
           if (!div.textContent) {
             div.textContent = 'o';
             turn = 'x';
-            array.push({position: div.id, value: div.textContent});
+            array.splice(div.id, 1, div.textContent);
           };
         };
-        //TODO: figure out how to check result
-        console.log(array);
+        if (checkResult(array)) {
+          console.log(winner);
+          result.textContent = winner + ' wins!';
+          return clearBoard();
+        };
       });
     });
   };
+  const checkResult = (array) => {
+    const winningCombinations = [
+      array.slice(0, 3),
+      array.slice(3, 6),
+      array.slice(6),
+      [array[0], array[3], array[6]],
+      [array[1], array[4], array[7]],
+      [array[2], array[5], array[8]],
+      [array[0], array[4], array[8]],
+      [array[2], array[4], array[6]]
+      ];
+    return winningCombinations.some(combination => {
+      if (combination.every(el => el !== '') &&
+          combination.every(el => el === combination[0])) {
+        winner = combination[0];
+        //console.log(winningCombinations.indexOf(combination));
+        //TODO: change text color of winning combination on the board
+        return true;
+      };
+    });
+  };
+  const clearBoard = () => {
+    array = ['', '', '', '', '', '', '', '', ''];
+    board.forEach(div => {div.textContent = ''})
+  }
   return {
-    array,
-    move
+    play
   };
 })();
 
 //factory function
 const Player = () => {
-  //const getName  = () => name;
+  //TODO: write function choosing player
   const turn = () => console.log('Player first!');
-  return { name, turn };
+  return { turn };
 };
 
-Gameboard.move('x');
+//module Computer
+const Computer = (() => {
+  const move = 
+  return { move };
+})();
+
+Gameboard.play('x');
